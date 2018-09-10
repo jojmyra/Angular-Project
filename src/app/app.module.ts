9213d2afc2efebea39c0f49e158ec50b3a727bd3
59160548 <59160548@go.buu.ac.th>
2018-09-10 18:56:14 +0700

    add hammerjs add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { TemplateSidebarComponent } from './template-sidebar/template-sidebar.component';
+import { TemplateHeaderComponent } from './template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateSidebarComponent,
+    TemplateHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
